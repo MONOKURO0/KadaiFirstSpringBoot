66ccf30765448aba1b8c34b6426560192c6d4b3c
MONOKURO0 <kurousagi-kerotan@live.jp>
2023-12-23 11:15:34 +0900

    初期設定

---
+++ src/main/java/com/kadai14/KadaiFirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kadai14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadaiFirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadaiFirstSpringBootApplication.class, args);
+	}
+
+}
